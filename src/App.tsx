165a5cb4c43f673944a9f6cc59811eb6033e7aad
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2024-08-04 22:33:43 +0900

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import CharacterCount from "@tiptap/extension-character-count";
+import ImageExtension from "@tiptap/extension-image";
+import { Editor } from "@tiptap/core";
 import { memo, useState } from "react";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import "./App.css";
 
-const extensions = [StarterKit, CharacterCount];
+const extensions = [StarterKit, CharacterCount, ImageExtension];
 
 const content = "<p>Hello World!</p>";
 
 const CharacterCounter = memo(({ charCount }: { charCount: number }) => {
-  return <p>{charCount}</p>;
+  return <p>文字数：{charCount}</p>;
 });
+
+const ImageCounter = ({ imageCount }: { imageCount: number }) => {
+  return <p>画像枚数：{imageCount}</p>;
+};
+
+function collectImageCount(editor: Editor) {
+  let imageCount = 0;
+
+  editor.state.doc.descendants((node) => {
+    if (node.type.name === ImageExtension.name) {
+      imageCount++;
+    }
+  });
+
+  return imageCount;
+}
 
 function App() {
   const [charCount, setCharCount] = useState(0);
+  const [imageCount, setImageCount] = useState(0);
+
   const editor = useEditor({
     extensions,
     content,
     onUpdate({ editor }) {
       setCharCount(editor.storage.characterCount.characters());
+      setImageCount(collectImageCount(editor));
     },
   });
 
@@ -28,6 +49,7 @@ function App() {
     <>
       <EditorContent editor={editor} />
       <CharacterCounter charCount={charCount} />
+      <ImageCounter imageCount={imageCount} />
       <button
         type="button"
         onClick={() => {
@@ -35,6 +57,19 @@ function App() {
         }}
       >
         コンテンツを更新
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          editor
+            .chain()
+            .focus()
+            .setTextSelection(0)
+            .setImage({ src: "https://placehold.jp/150x150.png" })
+            .run();
+        }}
+      >
+        画像を追加する
       </button>
     </>
   );
